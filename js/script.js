4eade4e96af985dd0c43e3c6a3f4b452c911683b
iramilyukova <60115026+iramilyukova@users.noreply.github.com>
2020-03-12 00:15:58 +0300

    Проверка анимации формы

--- js/script.js
+++ js/script.js
@@ -33,7 +33,8 @@ button.addEventListener("click", function (evt) {
     else{
       child.focus();
     }
-  } else {
+  }
+  else {
     adults.focus();
   }
 });
@@ -43,13 +44,24 @@ form.addEventListener("submit", function (evt) {
   if (appointment.value && departure.value && isStorageSupport) {
     localStorage.setItem("adults", adults.value);
     localStorage.setItem("child", child.value);
-    inputOrderText.classList.remove("field-error");
-  } else {
+
+    appointment.classList.remove("field-error");
+    departure.classList.remove("field-error");
+  }
+  else {
     evt.preventDefault();
+
+    if(appointment.value === '') {
+      appointment.classList.add("field-error");
+    }
+
+    if(departure.value === '') {
+      departure.classList.add("field-error");
+    }
+
     popup.classList.remove("form-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("form-error");
-    inputOrderText.classList.add("field-error");
     console.log("Нужно ввести даты заезда");
   }
 });
